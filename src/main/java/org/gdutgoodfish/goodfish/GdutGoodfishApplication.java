07beb9836837cc58f0ab3208319fa2dae5bdb1d1
15517 <1551758442@qq.com>
2024-11-11 12:25:05 +0800

    feat:
    初始项目代码

---
+++ src/main/java/org/gdutgoodfish/goodfish/GdutGoodfishApplication.java
@@ -0,0 +1,13 @@
+package org.gdutgoodfish.goodfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdutGoodfishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdutGoodfishApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gdutgoodfish/goodfish/GdutGoodfishApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gdutgoodfish.goodfish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdutGoodfishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
