4504b983b1a1579baa4ae7da9bb3794581178fdf
J <J.@qq.com>
2024-11-12 17:01:51 +0800

    fix:修改隐私

--- src/main/java/org/gdutgoodfish/goodfish/util/FileUploadUtil.java
+++ src/main/java/org/gdutgoodfish/goodfish/util/FileUploadUtil.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 @Component
 @Data
-@ConfigurationProperties(prefix = "oss")
+@ConfigurationProperties(prefix = "aliyunoss")
 public class FileUploadUtil {
 
     private String endpoint;
