b6f0cb1fac9669da03c66f28f00ce35114875338
钟启腾 <3044227626@qq.com>
2024-11-15 21:40:50 +0800

    发送消息功能开发

--- src/main/java/org/gdutgoodfish/goodfish/controller/MessageController.java
+++ src/main/java/org/gdutgoodfish/goodfish/controller/MessageController.java
@@ -1,8 +1,13 @@
 package org.gdutgoodfish.goodfish.controller;
 
 
+import org.gdutgoodfish.goodfish.pojo.common.Result;
+import org.gdutgoodfish.goodfish.pojo.dto.MessageAddDTO;
+import org.gdutgoodfish.goodfish.service.IMessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,4 +22,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/message")
 public class MessageController {
 
+    @Autowired
+    IMessageService messageService;
+
+    /**
+     * 新增消息
+     * @param messageAddDTO
+     * @return
+     */
+    @PostMapping("/add")
+    public Result addMessage(@RequestBody MessageAddDTO messageAddDTO) {
+        // 存入message
+        boolean success = messageService.addMessage(messageAddDTO);
+        // 返回结果
+        if (success) {
+            return Result.success("发送成功");
+        } else {
+            return Result.error("发送失败");
+        }
+    }
 }
--- src/main/java/org/gdutgoodfish/goodfish/service/impl/MessageServiceImpl.java
+++ src/main/java/org/gdutgoodfish/goodfish/service/impl/MessageServiceImpl.java
@@ -1,10 +1,15 @@
 package org.gdutgoodfish.goodfish.service.impl;
 
-import org.gdutgoodfish.goodfish.pojo.entity.Message;
-import org.gdutgoodfish.goodfish.mapper.MessageMapper;
-import org.gdutgoodfish.goodfish.service.IMessageService;
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.gdutgoodfish.goodfish.mapper.MessageMapper;
+import org.gdutgoodfish.goodfish.pojo.common.UserContext;
+import org.gdutgoodfish.goodfish.pojo.dto.MessageAddDTO;
+import org.gdutgoodfish.goodfish.pojo.entity.Message;
+import org.gdutgoodfish.goodfish.service.IMessageService;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -16,5 +21,21 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MessageServiceImpl extends ServiceImpl<MessageMapper, Message> implements IMessageService {
+
+    @Override
+    public boolean addMessage(MessageAddDTO messageAddDTO) {
+        // 从messageAddDTO取出参数复制到message实体
+        Message message = BeanUtil.copyProperties(messageAddDTO, Message.class);
+        // 往message添加默认值
+        Long sendId = UserContext.getCurrentId();
+        LocalDateTime createTime = LocalDateTime.now();
+        Integer isRead = 0;
+        message.setSendId(sendId);
+        message.setCreateTime(createTime);
+        message.setIsRead(isRead);
+        // 存入message并返回结果
+        return this.save(message);
+    }
+
 
 }
